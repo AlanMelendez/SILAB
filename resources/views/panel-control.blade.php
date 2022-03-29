53142b78f8b2d67b2d404464170c3c6e5d51dc1c
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2022-03-29 12:39:30 -0600

    Agregando ADMINLTE

---
+++ resources/views/panel-control.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'SILAB')
+
+@section('content_header')
+    <h1>SILAB</h1>
+@stop
+
+@section('content')
+    <h1>Contenido de nuestro sitio! </h1>
+    
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
