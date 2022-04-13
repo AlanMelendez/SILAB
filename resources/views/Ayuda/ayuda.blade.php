5a323667b10e0347522cbbf938bb8c3e2cbbe6ad
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2022-04-12 21:08:48 -0500

    Panel ayuda modificado

--- resources/views/Ayuda/ayuda.blade.php
+++ resources/views/Ayuda/ayuda.blade.php
@@ -6,8 +6,23 @@
    
 @stop
 @section('content')
-    <div class="container header">
-        <h1>AYUDA</h1>
+    <div class="container header-ayuda">
+        <div class="logo-header">
+            <img class="logo-ayuda" src="/img/logo-ayuda.png" alt="logo-ayuda">
+        </div>
+        <div class="container-titulo-header">
+            <h2 class="titulo-header">¿Como podemos ayudarte?</h2>
+        </div>
+        <div class="barra-ayuda">
+            <form action="" class="barra-ayuda-header">
+                <button class="btn-buscarBarra">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                      </svg>
+                </button>
+                <input class="input-barrabuscar" type="search" placeholder="Busca aqui mas soluciones de ayuda" aria-label="Search">
+            </form>
+        </div>
         <p class="textoAyuda">
             Aqui encontraras informacion que pouede ayudarte a utilizar la plataforma, Iniciar algun tramite y/o servicio.
         </p>
@@ -36,7 +51,14 @@
                                         </p>
                                         <div class="collapse" id="collapsePreguntas1">
                                             <div class="card card-body">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eaque, eos harum beatae nostrum aspernatur tempora sequi voluptatem ducimus ratione culpa vitae similique ad cum provident asperiores quos voluptate hic modi. Esse vero nostrum, facilis quo optio, voluptas sed possimus dicta officia ex obcaecati qui eaque corporis! Provident delectus sit eaque enim, suscipit qui consequatur, expedita deserunt voluptatum quisquam tempore natus dolor totam! Facere labore tempora quia distinctio quos nulla excepturi soluta sed dolore possimus commodi recusandae praesentium quasi, veritatis velit odit non exercitationem omnis minus esse et fuga. Debitis laboriosam, architecto asperiores in nulla quae error cum voluptates? Quia omnis, dicta, quasi rerum ea amet saepe sed quam maxime vitae earum officiis quo, autem soluta eius vero aliquam harum ipsa obcaecati molestiae tempore pariatur minus ipsam? Impedit sit amet culpa officia tenetur ipsam aliquam, libero asperiores animi enim. Recusandae, tenetur. Aliquam facere totam hic, deleniti quia minus voluptates, corporis distinctio natus accusamus voluptatum neque atque fugit consequuntur molestias eos fuga nisi. Saepe repudiandae ratione, praesentium nobis laborum eveniet non, nemo obcaecati quasi, voluptatibus vitae harum! Sunt, nulla! Nobis delectus molestiae animi autem eius ut ab dicta eaque ipsa dolor quibusdam commodi a, nostrum et cumque vitae molestias, cupiditate voluptates qui! Qui eveniet nihil omnis facilis consequuntur delectus sequi. In, natus corporis deleniti debitis a aspernatur itaque unde exercitationem adipisci animi hic rerum iure quidem consequatur ipsum labore quis, omnis fuga quibusdam saepe voluptate obcaecati ipsa praesentium! Id, possimus blanditiis.
+                                                <p>Esta pagina fue creada principalmente para ahorrar tiempos ala hora solicitar tu carta de no adeudo.</p>
+                                                <p>Dentro de la pagina podras:</p>
+                                                    <ul>
+                                                        <li class="lista-adeudos">Iniciar un tramite de carta de no adeudo</li>
+                                                        <li class="lista-adeudos">Verificar el estatus de tu tramite</li>
+                                                        <li class="lista-adeudos">Visualizar adeudos activos.</li>
+                                                        <li class="lista-adeudos">Visualizar registros de todos tus adeudos.</li>
+                                                    </ul>
                                             </div>
                                         </div>
                                     </div>
@@ -49,7 +71,9 @@
                                         </p>
                                         <div class="collapse" id="collapsePreguntas2">
                                             <div class="card card-body">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eaque, eos harum beatae nostrum aspernatur tempora sequi voluptatem ducimus ratione culpa vitae similique ad cum provident asperiores quos voluptate hic modi. Esse vero nostrum, facilis quo optio, voluptas sed possimus dicta officia ex obcaecati qui eaque corporis! Provident delectus sit eaque enim, suscipit qui consequatur, expedita deserunt voluptatum quisquam tempore natus dolor totam! Facere labore tempora quia distinctio quos nulla excepturi soluta sed dolore possimus commodi recusandae praesentium quasi, veritatis velit odit non exercitationem omnis minus esse et fuga. Debitis laboriosam, architecto asperiores in nulla quae error cum voluptates? Quia omnis, dicta, quasi rerum ea amet saepe sed quam maxime vitae earum officiis quo, autem soluta eius vero aliquam harum ipsa obcaecati molestiae tempore pariatur minus ipsam? Impedit sit amet culpa officia tenetur ipsam aliquam, libero asperiores animi enim. Recusandae, tenetur. Aliquam facere totam hic, deleniti quia minus voluptates, corporis distinctio natus accusamus voluptatum neque atque fugit consequuntur molestias eos fuga nisi. Saepe repudiandae ratione, praesentium nobis laborum eveniet non, nemo obcaecati quasi, voluptatibus vitae harum! Sunt, nulla! Nobis delectus molestiae animi autem eius ut ab dicta eaque ipsa dolor quibusdam commodi a, nostrum et cumque vitae molestias, cupiditate voluptates qui! Qui eveniet nihil omnis facilis consequuntur delectus sequi. In, natus corporis deleniti debitis a aspernatur itaque unde exercitationem adipisci animi hic rerum iure quidem consequatur ipsum labore quis, omnis fuga quibusdam saepe voluptate obcaecati ipsa praesentium! Id, possimus blanditiis.
+                                                <p></p>
+                                                <p></p>
+                                                <p></p>
                                             </div>
                                         </div>
                                     </div>
@@ -108,7 +132,7 @@
                 <div class="parrafoColapso col-12">
                     <p>
                     
-                        <button class="btn btn-primary col-12" type="button" data-toggle="collapse" data-target="#collapsePoliticas0" aria-expanded="false" aria-controls="collapsePoliticas">
+                        <button class="btn btn-primary col-12" type="button" data-toggle="collapse" data-target="#collapsePoliticas0" aria-expanded="false" aria-controls="collapseTerminos">
                             Politicas y seguridad
                         </button>
                     </p>
@@ -154,7 +178,7 @@
                                         </p>
                                         <div class="collapse" id="collapsePoliticas3">
                                             <div class="card card-body">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eaque, eos harum beatae nostrum aspernatur tempora sequi voluptatem ducimus ratione culpa vitae similique ad cum provident asperiores quos voluptate hic modi. Esse vero nostrum, facilis quo optio, voluptas sed possimus dicta officia ex obcaecati qui eaque corporis! Provident delectus sit eaque enim, suscipit qui consequatur, expedita deserunt voluptatum quisquam tempore natus dolor totam! Facere labore tempora quia distinctio quos nulla excepturi soluta sed dolore possimus commodi recusandae praesentium quasi, veritatis velit odit non exercitationem omnis minus esse et fuga. Debitis laboriosam, architecto asperiores in nulla quae error cum voluptates? Quia omnis, dicta, quasi rerum ea amet saepe sed quam maxime vitae earum officiis quo, autem soluta eius vero aliquam harum ipsa obcaecati molestiae tempore pariatur minus ipsam? Impedit sit amet culpa officia tenetur ipsam aliquam, libero asperiores animi enim. Recusandae, tenetur. Aliquam facere totam hic, deleniti quia minus voluptates, corporis distinctio natus accusamus voluptatum neque atque fugit consequuntur molestias eos fuga nisi. Saepe repudiandae ratione, praesentium nobis laborum eveniet non, nemo obcaecati quasi, voluptatibus vitae harum! Sunt, nulla! Nobis delectus molestiae animi autem eius ut ab dicta eaque ipsa dolor quibusdam commodi a, nostrum et cumque vitae molestias, cupiditate voluptates qui! Qui eveniet nihil omnis facilis consequuntur delectus sequi. In, natus corporis deleniti debitis a aspernatur itaque unde exercitationem adipisci animi hic rerum iure quidem consequatur ipsum labore quis, omnis fuga quibusdam saepe voluptate obcaecati ipsa praesentium! Id, possimus blanditiis.
+                                                Si no podrás continuar con tu inscripción semestral si cuentas con un adeudo en algún laboratorio o área correspondiente
                                             </div>
                                         </div>
                                     </div>
@@ -167,8 +191,7 @@
                                         </p>
                                         <div class="collapse" id="collapsePoliticas4">
                                             <div class="card card-body">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eaque, eos harum beatae nostrum aspernatur tempora sequi voluptatem ducimus ratione culpa vitae similique ad cum provident asperiores quos voluptate hic modi. Esse vero nostrum, facilis quo optio, voluptas sed possimus dicta officia ex obcaecati qui eaque corporis! Provident delectus sit eaque enim, suscipit qui consequatur, expedita deserunt voluptatum quisquam tempore natus dolor totam! Facere labore tempora quia distinctio quos nulla excepturi soluta sed dolore possimus commodi recusandae praesentium quasi, veritatis velit odit non exercitationem omnis minus esse et fuga. Debitis laboriosam, architecto asperiores in nulla quae error cum voluptates? Quia omnis, dicta, quasi rerum ea amet saepe sed quam maxime vitae earum officiis quo, autem soluta eius vero aliquam harum ipsa obcaecati molestiae tempore pariatur minus ipsam? Impedit sit amet culpa officia tenetur ipsam aliquam, libero asperiores animi enim. Recusandae, tenetur. Aliquam facere totam hic, deleniti quia minus voluptates, corporis distinctio natus accusamus voluptatum neque atque fugit consequuntur molestias eos fuga nisi. Saepe repudiandae ratione, praesentium nobis laborum eveniet non, nemo obcaecati quasi, voluptatibus vitae harum! Sunt, nulla! Nobis delectus molestiae animi autem eius ut ab dicta eaque ipsa dolor quibusdam commodi a, nostrum et cumque vitae molestias, cupiditate voluptates qui! Qui eveniet nihil omnis facilis consequuntur delectus sequi. In, natus corporis deleniti debitis a aspernatur itaque unde exercitationem adipisci animi hic rerum iure quidem consequatur ipsum labore quis, omnis fuga quibusdam saepe voluptate obcaecati ipsa praesentium! Id, possimus blanditiis.
-                                            </div>
+                                                Tu información tu nombre ,semestre, carrera, número control y  NO tener adeudos pendientes                                            </div>
                                         </div>
                                     </div>
                                     <div class="parrafoColapso col-12">
@@ -184,6 +207,51 @@
                                             </div>
                                         </div>
                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="preguntasfrecuentes col-12">
+                <div class="parrafoColapso col-12">
+                    <p>
+                    
+                        <button class="btn btn-primary col-12" type="button" data-toggle="collapse" data-target="#collapseTerminos0" aria-expanded="false" aria-controls="collapsePoliticas">
+                            Terminos y condiciones
+                        </button>
+                    </p>
+                    <div class="collapse" id="collapseTerminos0">
+                        <div class="card card-body">
+                            <div class="container-faq col-12 ">
+                                <div class="title-faq col-12">
+                                    
+                                    <div class="parrafoColapso col-12">
+                                        <div class="card card-body">
+                                            <p> 
+                                                 La utilización de este sitio y de cualquiera de las vista  del mismo constituye el pleno y expreso consentimiento por parte del usuario para observar y sujetarse respecto de cada uno de los términos y condiciones que aquí se contienen, así como respecto de las políticas de privacidad, políticas de seguridad y, en su caso, cualesquiera otros documentos que conformen parte o regulen la participación del usuario en este sitio. En el supuesto que cualquiera de los términos a los que habrá de sujetarse el usuario cuando acceda a este sitio sean contrarios a sus intereses, deberá abstenerse de hacer uso de este.
+                                            </p>
+                                            <p>
+                                                Obligaciones del usuario:
+                                                Al utilizar este sitio me obligo a cumplir con los términos y condiciones de su uso, establecidos en el presente documento (en adelante, los “términos y condiciones"), por lo que reconozco haber leído, entendido y estar de acuerdo en sujetarme a sus términos y condiciones. De manera que, al acceder al sitio, estoy de acuerdo en acatar lo anterior.
+                                            </p>
+
+                                            <p>
+                                                La información y los servicios que ofrece SILAB, (en adelante el Sitio) hacen referencia exclusivamente.
+
+                                            Por el hecho de hacer uso de la información y hacer uso de los Servicios, el usuario expresa su aceptación y está de acuerdo con los términos y condiciones del uso del sitio, así como con las modificaciones que, en su caso, se realicen.
+                                            </p>
+
+                                            
+                                        </div>
+                                        
+                                    </div>
+                                    
                                 </div>
                             </div>
                         </div>
