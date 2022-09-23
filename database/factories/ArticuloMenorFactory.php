596dad04237ec8440ee00801b1d9a7b3484d497f
Daniela Estala <estala373@gmail.com>
2022-09-23 12:49:06 -0500

    Modificación ArticuloMenorFactory

--- database/factories/ArticuloMenorFactory.php
+++ database/factories/ArticuloMenorFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(articulo_menor::class, function (Faker $faker) {
     return [
-        //
+        //campos tabla de articulo menor
         'nombre'=> $faker -> word(),
         'descripcion_articulo'=> $faker -> name(),
         'status'=> $faker -> boolean(),
