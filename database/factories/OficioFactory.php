cfc6b37ec2acbc2256a995cec3026025c4d46a3d
Monica Ornelas <79345718+MonicaReyesOrnelas@users.noreply.github.com>
2022-09-23 10:35:53 -0500

    Factory-Seeders Oficio/Tramite

---
+++ database/factories/OficioFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\oficio;
+use Faker\Generator as Faker;
+
+$factory->define(oficio::class, function (Faker $faker) {
+    return [
+        
+
+        
+
+    ];
+});
---
+++ database/factories/TramiteFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\tramite;
+use Faker\Generator as Faker;
+
+$factory->define(tramite::class, function (Faker $faker) {
+    return [
+        
+        
+
+    ];
+});
